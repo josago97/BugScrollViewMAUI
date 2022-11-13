fe40cf768579c397bbc6dcde7a7c4f129c59bf88
josago97 <josago2009@hotmail.es>
2022-11-14 00:13:06 +0100

    no message

---
+++ ScrollViewTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ScrollViewTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ScrollViewTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ScrollViewTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
